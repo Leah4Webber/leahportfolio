adb9c54f51ce9a85e4a3ef3339ec1839f835cc9f
Leah4Webber <lewebb25@gmail.com>
2017-07-21 14:34:20 -0400

    Update finalprojjava.js

--- finalprojjava.js
+++ finalprojjava.js
@@ -26,16 +26,16 @@ points=points+4;
 
 
 $("#submit").click(function(){
-    if (points<12){
+    if (points<13){
         $("#cat").attr("src", "http://s3.weddbook.com/t1/2/5/6/2569276/2-ct-engagement-ring-4-prong-classic-solitaire-ring-man-made-diamond-simulant-round-wedding-ring-sterling-silver-rose-gold-plated.jpg");
     }
-    else if (points >11 && points<19){
+    else if (points >12 && points<21){
         $("#cat").attr("src", "https://www.loveandpromisejewelers.com/media/catalog/product/cache/1/image/670x500/9df78eab33525d08d6e5fb8d27136e95/m/a/mae_9x7mm_14kt_rose_gold_oval_fb_moissanite_and_diamond_halo_with_milgrain_engagement_ring_3.jpg");
     }
-    else if (points >18 && points<23){
+    else if (points >20 && points<26){
         $("#cat").attr("src", "https://www.diamondmansion.com/skin/frontend/meigeetheme/default/home/img/thumbnail-4.jpg");
     }
-    else if (points >22){
+    else if (points >25){
         $("#cat").attr("src", "https://s-media-cache-ak0.pinimg.com/736x/ae/15/37/ae1537c25ae0071bd09487722a23386d--rectangle-diamond-ring-rectangle-engagement-ring.jpg");
     }
 
